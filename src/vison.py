4b12e9ef7fc2a6fea3cf23e4e19ee51f58f912ed
Jonathan Tan <jonalogy@gmail.com>
2020-09-24 18:41:24 +0800

    Minor refactor to vision()

--- src/vison.py
+++ src/vison.py
@@ -4,10 +4,9 @@ import numpy as np
 
 
 def vision(save_img_to):
-  camera = PiCamera()
-  camera.resolution = (1024, 768)
-  
-  camera.capture(str(save_img_to))
+  with PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    camera.capture(str(save_img_to))
 
 def load_and_resize_image(img_path, img_size=(224, 224)):
     img = Image.open(img_path).resize(img_size)
